55ab64bac1ae3d829d5ebdf0dd27dd515e246f60
HardPlant <abc7988se@naver.com>
2019-08-02 21:15:11 +0900

    Update baseball.unit.test.js

--- baseball/baseball.unit.test.js
+++ baseball/baseball.unit.test.js
@@ -1,9 +1,16 @@
-// 자신/상대 수 결정
-it("chooses player's number", function() {
+const fs = require("fs");
+eval(fs.readFileSync("baseball/baseball.js") + "");
 
+// 자신/상대 수 = 결정
+it("chooses player's number", function() {
+    var player_num = "123";
+    HP_Baseball_Game.choosePlayerNumber(player_num);
+    expect(HP_Baseball_Game._playerNumber).toStrictEqual(["1","2","3"]);
 });
 it("chooses enemys's number", function() {
-
+    var enemy_number = "123";
+    HP_Baseball_Game.chooseEnemyNumber(enemy_number);
+    expect(HP_Baseball_Game._enemyNumber).toStrictEqual(["1","2","3"]);
 });
 //턴 지정
 it("chooses turn", function() {
